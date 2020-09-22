acae20af00895938aff93a83803505e5f3952530
Gabriel de Jesus de <gabriel.franca@dce.ufpb.br>
2020-09-22 19:38:19 +0000

    Funcionando.

--- cupom.test.ts
+++ cupom.test.ts
@@ -73,21 +73,26 @@ test('Inscrição estadual vazia', () => {
 test('Exercício 2 - customizado', () => {
 
   // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
+	cupom.dados.nome_loja = "Tropical";
+  cupom.dados.logradouro = "Rua siqueira Campos";
+  cupom.dados.numero = 580;
   cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
+  cupom.dados.bairro = "Centro";
+  cupom.dados.municipio = "Paulista";
+  cupom.dados.estado = "Pernambuco";
+  cupom.dados.cep = "53401-320";
+  cupom.dados.telefone = "(81) 3438-5714";
   cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+  cupom.dados.cnpj = "37.886.772/0001-82";
+  cupom.dados.inscricao_estadual = "4232303-79";
 
   //E atualize o texto esperado abaixo
-  expect(cupom.dados_loja()).toBe(
-    `
+  expect(cupom.dados_loja()).toBe(`Tropical
+Rua siqueira Campos, 580
+Centro - Paulista - Pernambuco
+CEP:53401-320 Tel (81) 3438-5714
+
+CNPJ: 37.886.772/0001-82
+IE: 4232303-79
 `);
 });
